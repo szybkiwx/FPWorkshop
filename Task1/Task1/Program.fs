3326c7a013ea50ae1d362dc14a43384a4920865d
Andrzej Dajnowski <andrzej.dajnowski@rikstv.no>
2020-02-04 21:38:41 +0100

    moving task1 to task2, changing task1

---
+++ Task1/Task1/Program.fs
@@ -0,0 +1,21 @@
+﻿open System.IO
+
+let readLines (filePath:string) = seq {
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
+let readData() = readLines "input.txt" |> Seq.map int |> Seq.toList
+
+let part1 data = 0
+
+let part2 data = 0
+
+
+[<EntryPoint>]
+let main argv =
+    let data = readData()
+    let result1 = part1 data
+    let result2 = part2 data
+    0 // return an integer exit code

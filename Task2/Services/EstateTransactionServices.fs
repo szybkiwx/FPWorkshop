bd89e4d697547689bc743923782d01c76c456169
Andrzej Dajnowski <andrzej.dajnowski@rikstv.no>
2020-02-04 20:38:12 +0100

    solution Task1

--- Task2/Services/EstateTransactionServices.fs
+++ Task2/Services/EstateTransactionServices.fs
@@ -6,13 +6,21 @@ module EstateTransactionServices =
     open Model
 
     let GetAverageResidentialArealInCity (city:string) (transactions: seq<EstateTransaction>) =
-        0.0
+         List.ofSeq(transactions)
+            |> List.filter (fun x -> x.City = city)
+            |> List.map (fun x -> float x.SqFeet)
+            |> List.average
 
     let GetTotalTransactionAmountFromAreaByZip (zip:string) (transactions: seq<EstateTransaction>) = 
-        0m
+        List.ofSeq(transactions)
+            |> List.filter(fun x -> x.ZipCode = zip)
+            |> List.sumBy (fun x -> decimal x.Price) 
 
     let GetAverageBedroomsSoldInBetweenDates (date1:DateTime) (date2:DateTime) (transactions: seq<EstateTransaction>) = 
-        0.0
+        List.ofSeq(transactions)
+            |> List.filter (fun x -> x.SaleDate > date1 && x.SaleDate < date2)
+            |> List.map (fun x -> float x.Beds)
+            |> List.average
 
     let GetAveragePricePerSquareFeetByCity (transactions: seq<EstateTransaction>)  =
         Map.empty<string, float>

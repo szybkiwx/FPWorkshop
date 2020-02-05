428125b1865b912ad9cb4af4dc6eae9f00899384
Andrzej Dajnowski <andrzej.dajnowski@rikstv.no>
2020-02-05 15:31:25 +0100

    updating solution to task1

--- Task1/Task1/Program.fs
+++ Task1/Task1/Program.fs
@@ -8,18 +8,22 @@ let readLines (filePath:string) = seq {
 
 let readData fileName = readLines fileName |> Seq.map int
 
-let part1 data = data |> Seq.map int |> Seq.sum
+let calcFuel mass =
+    let fl = (float mass) / 3.0 |> floor |> int
+    int (fl-2)
 
-let part2 data = 
-    let rec find input acc history =
-        match input with
-            | [] -> find data acc history
-            | head::rest -> if List.contains acc history then 
-                                acc 
-                            else 
-                                find rest (acc+head) (acc::history)
+let part1 input = input |> Seq.map calcFuel |> Seq.sum
 
-    find data 0 []
+let calcMoreFuel mass =
+    let rec loop inputMass result =
+        let nextMass = calcFuel inputMass
+        if nextMass <= 0 then 
+            result 
+        else 
+            loop nextMass (result + nextMass) 
+    loop mass 0  
+
+let part2 input = input |> Seq.map calcMoreFuel |> Seq.sum
 
 [<EntryPoint>]
 let main argv =

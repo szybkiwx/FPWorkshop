052e20d040a7bbe47637269ab0e908391086ec8c
Andrzej Dajnowski <andrzej.dajnowski@rikstv.no>
2020-02-05 12:59:16 +0100

    updated Task1

--- Task1/Task1/Program.fs
+++ Task1/Task1/Program.fs
@@ -6,7 +6,7 @@ let readLines (filePath:string) = seq {
         yield sr.ReadLine ()
 }
 
-let readData() = readLines "input.txt" |> Seq.map int |> Seq.toList
+let readData fileName = readLines fileName |> Seq.map int
 
 let part1 data = 0
 
@@ -15,7 +15,10 @@ let part2 data = 0
 
 [<EntryPoint>]
 let main argv =
-    let data = readData()
+    let data = readData "input.txt"
     let result1 = part1 data
     let result2 = part2 data
+
+    printf "Part1 result: %d" result1
+    printf "Part2 result: %d" result2
     0 // return an integer exit code
